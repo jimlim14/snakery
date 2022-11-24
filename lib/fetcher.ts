246d565eba1ce42db71b15baa4eab8310a0bee0a
Jim <limchorngjim@gmail.com>
2022-11-24 14:40:02 +0100

    modified parameter data"s type

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,14 +1,20 @@
-export default async function fetcher (url: string, data = undefined) {
-  const res = await fetch(`/api/${url}`, {
-    method: data ? 'POST' : 'GET',
-    credentials: 'include',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data)
-  });
-  if (res.status > 299 && res.status < 200) {
-    throw new Error();
-  }
-  return await res.json();
+import { SigninBody } from '../interface/SigninBody';
+import { SignupBody } from '../interface/SignupBody';
+
+export default async function fetcher(
+	url: string,
+	data: undefined | SignupBody | SigninBody
+) {
+	const res = await fetch(`/api/${url}`, {
+		method: data ? 'POST' : 'GET',
+		credentials: 'include',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(data),
+	});
+	if (res.status > 299 && res.status < 200) {
+		throw new Error();
+	}
+	return await res.json();
 }
