3c676d84a36a0790a7a425e5df5a29815b3c5ffa
Jim <limchorngjim@gmail.com>
2022-11-25 22:02:15 +0100

    added logout mode type

--- lib/mutations.ts
+++ lib/mutations.ts
@@ -2,6 +2,6 @@ import { SigninBody } from '../interface/SigninBody';
 import { SignupBody } from '../interface/SignupBody';
 import fetcher from './fetcher';
 
-export const auth = (mode: 'signin' | 'signup', body: SigninBody | SignupBody | undefined) => {
+export const auth = (mode: 'signin' | 'signup' | 'logout', body: SigninBody | SignupBody | undefined) => {
 	return fetcher(`${mode}`, body);
 };
