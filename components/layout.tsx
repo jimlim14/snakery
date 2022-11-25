947dab8079913efdc52323bf836294a8d8789de7
Jim <limchorngjim@gmail.com>
2022-11-25 09:32:52 +0100

    fixed Box"s width for header component

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,16 +1,26 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Flex, ButtonGroup, Button, Spacer } from '@chakra-ui/react';
 import { Layout } from '../interface/Layout';
+import Header from './header';
 
 const Layout = ({ children }: Layout) => {
 	return (
 		<Box width='100vw' height='100vh'>
-			<Box position='absolute' top='0' left='0' height='150px' bg='red'>
-				header
+			<Box
+				position='absolute'
+				top='0'
+				left='0'
+				height='150px'
+				width='100%'
+				bg='red'
+			>
+				<Header />
 			</Box>
 			<Box position='absolute' top='150px' left='0' bg='green'>
 				sidebar
 			</Box>
-			<Box position='absolute' left='100px' top='150px'>{children}</Box>
+			<Box position='absolute' left='100px' top='150px'>
+				{children}
+			</Box>
 			<Box position='absolute' bottom='0' height='150px' bg='yellow'>
 				footer
 			</Box>
