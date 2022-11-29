517e863aa41b311516654cbdaf1c08c1bdc51e66
Jim <limchorngjim@gmail.com>
2022-11-29 15:11:16 +0100

    added LogoutBody interface to body

--- lib/mutations.ts
+++ lib/mutations.ts
@@ -1,7 +1,8 @@
 import { SigninBody } from '../interface/SigninBody';
 import { SignupBody } from '../interface/SignupBody';
+import { LogoutBody } from '../interface/LogoutBody';
 import fetcher from './fetcher';
 
-export const auth = (mode: 'signin' | 'signup' | 'logout', body: SigninBody | SignupBody | undefined) => {
+export const auth = (mode: 'signin' | 'signup' | 'logout', body: SigninBody | SignupBody | LogoutBody | undefined) => {
 	return fetcher(`${mode}`, body);
 };
