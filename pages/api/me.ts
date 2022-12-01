e14233ac408665e41ae1472321569a2a1bd2f829
Jim <limchorngjim@gmail.com>
2022-12-01 15:45:21 +0100

    query user"s games after validation

--- pages/api/me.ts
+++ pages/api/me.ts
@@ -4,6 +4,10 @@ import { validateRoute } from '../../lib/auth';
 
 export default validateRoute(
 	async (req: NextApiRequest, res: NextApiResponse, user: any) => {
-		res.json(user);
+		const userGames = await prisma.user.findUnique({
+			where: { id: user.id },
+			select: { games: true },
+		});
+		res.json({...user, ...userGames});
 	}
 );
