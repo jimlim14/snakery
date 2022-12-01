907a1911f21e765438f51935c1585d53151849c9
Jim <limchorngjim@gmail.com>
2022-12-01 15:47:37 +0100

    solved 401 json reponse and commented useGames hook (might be useful)

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -2,11 +2,22 @@ import useSWR from 'swr';
 import fetcher from './fetcher';
 
 export const useMe = () => {
-	const { data, error } = useSWR('me', fetcher);
+	const { data, mutate, error } = useSWR('me', fetcher);
 
 	return {
 		user: data,
 		isLoading: !data && !error,
-		isError: error,
+		isError: data?.error || error,
+		mutate
 	};
 };
+
+// export const useGames = () => {
+// 	const { data, error } = useSWR('game', fetcher);
+
+// 	return {
+// 		games: data,
+// 		isLoading: !data && !error,
+// 		isError: error,
+// 	};
+// };
