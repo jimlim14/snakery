f0d21c7733ff8491361553aefb5164f743f6ba8a
Jim <limchorngjim@gmail.com>
2022-12-01 15:48:26 +0100

    tested login and logout for data fetching

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,10 +1,24 @@
 import { Box } from "@chakra-ui/react";
+import { useMe } from "../lib/hooks";
 
 const Sidebar = () => {
+  const { user, isLoading, isError } = useMe();
+
+  function test() {
+    if (isLoading) return <Box>loading</Box>
+    if (isError) return <Box>something is wrong</Box> 
+    return user.games.map((game: any) => (
+      <Box key={game.id}>
+        {game.difficulty}
+      </Box>
+    ))
+  }
+
   return (
     <Box height='100%'>
       sidebar
-    </Box>  
+      {test()}
+    </Box>
   )
 }
 
